fc6f1383d82ea4ff1fba4cd8cf40ea08dccc7299
Carlos Pavajeau <cantte098@gmail.com>
2024-07-06 18:41:19 -0500

    chore: Add types for Site, Metadata, and Socials

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type Site = {
+  name: string
+  email: string
+  postsOnHomePage: number
+  worksOnHomePage: number
+  projectsOnHomePage: number
+}
+
+export type Metadata = {
+  title: string
+  description: string
+}
+
+export type Socials = {
+  name: string
+  url: string
+}[]
