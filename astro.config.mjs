0a43d0d33e27c32a0f5719f58681e9beaa364dae
Carlos Pavajeau <cantte098@gmail.com>
2024-07-06 19:33:24 -0500

    chore: Update Astro config to include site URL and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://carlos-pavajeau.cantte.com/',
   integrations: [tailwind(), sitemap(), mdx()],
 })
